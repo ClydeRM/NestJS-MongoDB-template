c4ea814ccd2c6d3460c1f75b153c52255bcdc9cb
ClydeRM <clyde.official@icloud.com>
2021-12-27 17:24:50 +0800

    Import(Config/Mongooes) connect to MongoAtlas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.DBConnect.env' }), // Include .env variable
+    MongooseModule.forRoot(process.env.Mongo_URI), // Use env Mongo_URI
+  ],
   controllers: [],
   providers: [],
 })

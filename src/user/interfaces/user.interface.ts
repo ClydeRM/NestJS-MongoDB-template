c8fa2e523505b5ca6ca9277230be3108feba0f5f
ClydeRM <clyde.official@icloud.com>
2021-12-31 17:03:39 +0800

    (Create: ResponseDto) create Response instance

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+// 定義Response回傳物件屬性
+export interface User {
+  userId: string;
+  googleId: string;
+  displayName: string;
+  firstName: string;
+  lastName: string;
+  image: string;
+  createdAt: Date;
+}

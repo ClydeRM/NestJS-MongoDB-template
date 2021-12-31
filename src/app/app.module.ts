59c413915e9820d22735e8454b4ed3c0e0300a69
ClydeRM <clyde.official@icloud.com>
2021-12-31 17:04:28 +0800

    (Import: mongo config UserModule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.DBConnect.env' }), // Include .env variable
     MongooseModule.forRoot(process.env.Mongo_URI), // Use env Mongo_URI
+    UserModule,
   ],
   controllers: [],
   providers: [],
